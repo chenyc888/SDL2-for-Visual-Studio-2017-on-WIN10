c74c1c0f2a40ff5afb33c339d988855bb7336449
chenyc888 <33816401+chenyc888@users.noreply.github.com>
2017-11-28 16:30:28 -0800

    Add files via upload

---
+++ 59 - A Basic SDL Program.cpp
@@ -0,0 +1,14 @@
+// 59 - A Basic SDL Program.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "SDL.h"
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
